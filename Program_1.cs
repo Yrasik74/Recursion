8e7e6ef8cb5fa3c1deb05640a05a1b2a92cfce31
Yrasik74 <149587199+Yrasik74@users.noreply.github.com>
2023-12-12 13:31:47 +0300

    Add files via upload

---
+++ Program_1.cs
@@ -0,0 +1,24 @@
+﻿//Задача 1: Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+
+using System;
+
+class Program
+{
+static void Main()
+{
+int m = 1; // Значение M
+int n = 10; // Значение N
+PrintNaturalNumbers(m, n);
+}
+
+static void PrintNaturalNumbers(int m, int n)
+{
+if (m <= n)
+{
+Console.WriteLine(m);
+PrintNaturalNumbers(m + 1, n);
+}
+}
+}
